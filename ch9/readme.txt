e7c42d83c9f132b6bd836ae3234c483421781e58
Ash Blue <ash@ashbluewebdesign.com>
2012-05-19 00:30:02 -0500

    Moved GD from experiments to the actual folder and replaced the previous application.

--- ch9/readme.txt
+++ ch9/readme.txt
@@ -1,16 +1,17 @@
-For chapter 9 you'll find two versions of the WebGL engine.
-1. gl_engine = 'A completed working example of the entire chapter. Meant as a companion to chapter 9.';
-2. gl_eninge_blank = 'Includes a completed copy of the engine without and run.js code, so you can fill
-it with whatever you like.';
+For chapter 9 you'll find a copy of the Geometry Destroyer (GD) application. 
+This app is built exclusively built for Google Chrome although it may work in other browsers.
 
-To run gl_engine:
-1. Navigate to the root.
+Does your computer support WebGL???
+In Chrome you can see the compatability details of your graphics card by typing into the 
+address bar chrome://gpu and pressing enter. Then look for WebGL under Graphics Feature Status.
+Just because it says your graphics card works doesn't mean that your browser will run the game
+smoothly or at all. But is a good indicator for support. Some people have hacked their cards
+to get this working. But this is not recommended and if you blow up your computer its your own
+fault.
+
+To run GD:
+1. Navigate into the geometry_destroyer file
 2. Load up index.html and you should be good to go (Chrome recommended)
-
-To run gl_eninge_blank:
-1. Navigate to root->js->run.js
-2. Fill it out with your WebGL objects, setup your engine, and anything else you need to do.
-3. Navigate to root->index.html and run the file. All should work as expected (Chrome recommended).
 
 NOTE: If you aren't seeing any WebGL in Chrome try another computer with a better graphics card.
 Reason being graphics cards released in the past couple years are the only GPUs with the necessary drivers.
---
+++ experiments/ch9/geometry_destroyer/js/engine/template.js
@@ -0,0 +1,111 @@
+/*
+Name: Entity Templates
+Version: 1.0
+Author: Ashton Blue
+Author URL: http://blueashes.com
+Publisher: Manning
+Credits: Uses a modified version of John Resig's class extension script http://ejohn.org/blog/simple-javascript-inheritance/
+
+Desc: Storage location for all classes.
+*/
+
+var gd = gd || {};
+
+// Initialize and create an object so other classes can be stored here
+gd.template = {
+    Entity: Class.extend({
+        // Passive = 0, friendly a, enemy b
+        type: 0,
+        
+        // Determines position
+        x: 0,
+        y: 0,
+        z: 0,
+        
+        // Creates an artifical zoom without a complex transformation matrix or camera
+        zoom: -80,
+        
+        // Returns x, y, z in an array
+        position: function() {
+            return [this.x, this.y, this.z + this.zoom];
+        },
+        
+        // Width and height relative to 3D world, manually set
+        width: 0,
+        height: 0,
+        
+        update: function() {
+            // place code before animating here
+        },
+        
+        // Passes the object hit during a collision for processing
+        collide: function(object) {
+            this.kill();
+        },
+        
+        // Logic fired at object destruction
+        kill: function() {
+            gd.core.graveyard.storage.push(this);
+        },
+        
+        // Rotation information, used in core.js
+        rotate: {
+            angle: 0,
+            axis: false
+        },
+        
+        shape: function(vertices) {
+            this.shapeStorage = gd.gl.createBuffer();
+
+            // Graphic storage
+            gd.gl.bindBuffer(gd.gl.ARRAY_BUFFER, this.shapeStorage);
+            
+            // Uses float32 to change the array into a webGL edible format.
+            gd.gl.bufferData(gd.gl.ARRAY_BUFFER, new Float32Array(vertices), gd.gl.STATIC_DRAW);
+            
+            // Count rows
+            this.shapeColumns = 3;
+            this.shapeRows = vertices.length / this.shapeColumns;
+        },
+        
+        color: function(vertices) {
+            this.colorStorage = gd.gl.createBuffer();
+            
+            // Map colors for a complex object such as a cube, before doing so, check if the first array element is a string
+            // as it should be an array
+            if (typeof vertices[0] === 'object') {
+                // temporary storage location for new vertices
+                var colorNew = [];
+                
+                // Create complete verticy array
+                for (var v = 0; v < vertices.length; v++) {
+                    var colorLine = vertices[v];
+                    for (var c = 0; c < 4; c++) {
+                        colorNew = colorNew.concat(colorLine);
+                    }
+                }
+                
+                // Apply new verticy array
+                vertices = colorNew;                
+            }
+            
+            // Bind buffers as buffer.shape
+            gd.gl.bindBuffer(gd.gl.ARRAY_BUFFER, this.colorStorage);
+            gd.gl.bufferData(gd.gl.ARRAY_BUFFER, new Float32Array(vertices), gd.gl.STATIC_DRAW);
+
+            // Count rows
+            this.colorColumns = 4;
+            this.colorRows = vertices.length / this.colorColumns;
+        },
+
+        // Note: I don't think dimension is accurate, as I believe this just connects 2 triangles together, maybe snap is a better name?
+        dimension: function(vertices) {
+            this.dimensionStorage = gd.gl.createBuffer();
+            gd.gl.bindBuffer(gd.gl.ELEMENT_ARRAY_BUFFER, this.dimensionStorage);
+            gd.gl.bufferData(gd.gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(vertices), gd.gl.STATIC_DRAW);
+            
+            // Important, drawing with a dimension buffer combines triangles, so you're drawing with half the normal amount
+            this.dimensionCount = vertices.length;
+        }
+    })
+};
